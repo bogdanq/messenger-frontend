0b230377d8287af70fa739fe0e43893d862a4878
bogdan <bshelomanov98@mail.ru>
2019-09-24 21:49:34 +0300

    create theme context

---
+++ src/lib/theme-context.js
@@ -0,0 +1,26 @@
+import React, { createContext } from 'react'
+import { ThemeProvider } from 'styled-components'
+
+export const ThemeContext = createContext()
+export const ThemeProvider = ({ children, themes, initialTheme }) => {
+  const [theme, setTheme] = React.useState(themes[initialTheme])
+
+  const themeSetter = React.useCallback(
+    themeName => {
+      if (themes[themeName]) {
+        setTheme(themes[themeName])
+      }
+    },
+    [themes],
+  )
+
+  return (
+    <ThemeContext.Provider
+      values={{
+        themeSetter,
+      }}
+    >
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
